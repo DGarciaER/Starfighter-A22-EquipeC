4141666bf73a32afa799610f20b7732fcb8d75e5
AurelienLeFrancais <94388831+AurelienLeFrancais@users.noreply.github.com>
2022-11-10 14:26:55 -0500

    Update VueJeu.py

---
+++ VueJeu.py
@@ -0,0 +1,16 @@
+from tkinter import Canvas
+
+class VueJeu:
+
+    def afficherCarreRouge(self, carreRouge):
+        """Cette méthode affiche le carré rouge."""
+        carreRouge.draw()
+        
+    
+             
+    def clear(self, container):
+        """cette methose suprime la fenetre tout ce qui est dans la fenetre"""
+        container = container.grid_slaves()
+        for widget in container:
+            widget.destroy()
+    
